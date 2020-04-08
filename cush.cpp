22e7eb686c7edb7ab67b9eed776e8e7aac8d20b3
Aditya <adityakr082@gmail.com>
2020-04-08 20:21:50 +0530

    conversion in progress

--- cush.cpp
+++ cush.cpp
@@ -1,44 +1,45 @@
-// SPOILER: It works!
-
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <readline/readline.h>
 #include <readline/history.h>
 
-#define MAXCOM 1000 // max number of letters to be supported
-#define MAXLIST 100 // max number of commands to be supported
+const int MAXCOM = 1000; // max number of letters to be supported
+const int MAXLIST = 100; // max number of commands to be supported
 
 // Clearing the shell using escape sequences
-#define clear() printf("\033[H\033[J")
+inline void clear()
+{
+	std::cout << "\033[H\033[J";
+}
 
 // Greeting the shell during startup
 void init_shell()
 {
 	clear();
-	printf("***********\n"
-		   "hush	   \n"
-		"***********\n");
-	char *username = getenv("USER");
-	printf("\nUSER is: @%s", username);	
-	printf("\n");
+	std::cout << "************\n"
+		      << "    cush    \n"
+		      << "************\n";
+	std::string username = getenv("USER");
+	std::cout << "\nUSER is " << username << "\n";
 	sleep(1);
 	clear();
 }
 
 // Function to take input
-int takeInput(char* str)
+int takeInput(std::string str)
 {
-	char *buf;
+	std::string buf;
 
 	buf = readline("\n>>> ");
-	if (strlen(buf) != 0)
+	if (buf.length())
 	{
-		add_history(buf);
-		strcpy(str, buf);
+//		add_history(buf); // currently throws error
+		str = buf;
 		return 0;
 	}
 	else
